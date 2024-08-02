18e0d89e40bcaa49d80c5cd1937e7f591bf81c1c
zepi <eli.serra173@gmail.com>
2024-08-02 14:05:33 +0200

    Add init version

--- emon_worker_m8/__init__.py
+++ emon_worker_m8/__init__.py
@@ -1,1 +1,13 @@
 ﻿"""Emoncmms worker modules."""
+import logging
+
+__author__ = "Eli Serra"
+__copyright__ = "Copyright 2022, Eli Serra"
+__deprecated__ = False
+__license__ = "Apache"
+__status__ = "Production"
+__version__ = "0.0.1"
+
+
+logging.basicConfig()
+logger = logging.getLogger("vemonitor")
